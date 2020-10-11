193e1bfefeaa6df614452d72ce4787e8275dfe1e
unknown <laurentiu.bondor@gmail.com>
2020-10-11 15:34:19 +0300

    first commit

---
+++ bom.js
@@ -0,0 +1,22 @@
+console.log(window.location.href);
+
+const container = document.getElementById("container")
+
+const buton = document.createElement('button')
+buton.innerText = "schimbaUrl"
+buton.id = "btn"
+
+container.appendChild(buton)
+
+function schimbaUrl() {
+    console.log("ceva")
+
+
+    window.location.href = "https://google.com"
+
+
+
+
+}
+
+buton.addEventListener("click", schimbaUrl)
